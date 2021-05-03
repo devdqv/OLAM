b093107681754dae2e431c91d0b1cea4a2ce46e9
DOAN QUANG VINH <vinhqd>
2021-05-03 13:48:46 +0700

    tim theo ngay

--- OLAM/Controllers/HomeController.cs
+++ OLAM/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using PagedList;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,17 +11,44 @@ namespace OLAM.Controllers
 {
     public class HomeController : Controller
     {
+        // public Models.Saleman mUserInforBH;
+        public System.Globalization.CultureInfo objCultureInfo = new CultureInfo("vi-VN");
+
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult XuongTachVo(int page=1, int pageSize=6)
+        public ActionResult XuongTachVo(string startdate = "", string enddate = "", int page = 1, int pageSize = 6)
         {
+
             OLAMEntities db = new OLAMEntities();
-            ViewBag.PEELINGs = db.PEELINGs.OrderByDescending(x=>x.time_update).ToPagedList(page, pageSize);
+            var pEELINGs = db.PEELINGs.OrderByDescending(x => x.time_update);
+            DateTime datenow = DateTime.Now;
+            DateTime startD = new DateTime();
+            DateTime endD = new DateTime();
+            if (!string.IsNullOrEmpty(startdate) && !string.IsNullOrEmpty(enddate))
+            {
+                startD = Convert.ToDateTime(startdate, objCultureInfo);
+                ViewBag.startD = startD;
+                endD = Convert.ToDateTime(enddate, objCultureInfo);
+                ViewBag.endD = endD;
+
+
+            }
+            else
+            {
+                startD = new DateTime(datenow.Year, datenow.Month, datenow.Day, 0, 0, 0);
+                ViewBag.startD = startD;
+                endD = new DateTime(datenow.Year, datenow.Month, datenow.Day + 3, 23, 59, 59);
+                ViewBag.endD = endD;
+            }
+            ViewBag.PEELINGs = pEELINGs.Where(x => x.time_update <= endD && x.time_update >= startD).ToPagedList(page, pageSize);
+
+
+
             return View();
         }
 
-      
+
     }
 }
