3e816ec63c322a2ec752cf761e419ef224a1906c
DOAN QUANG VINH <vinhqd>
2021-05-03 14:51:04 +0700

    excel

--- OLAM/Controllers/HomeController.cs
+++ OLAM/Controllers/HomeController.cs
@@ -3,9 +3,14 @@ using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Syncfusion.XlsIO;
+
 
 namespace OLAM.Controllers
 {
@@ -78,6 +83,50 @@ namespace OLAM.Controllers
 
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ExportToExcel()
+        {
+            OLAMEntities db = new OLAMEntities();
+            var gv = new GridView();
+            gv.DataSource = db.PEELINGs.OrderByDescending(x => x.time_update).ToList().Select(x=> new{ x.ss_pressure, x.value_pressure, x.ss_speeddrum, x.Value_speeddrum, x.time_update});
+            gv.DataBind();
+            Response.Clear();
+            Response.Buffer = true;
+            //Response.AddHeader("content-disposition",
+            // "attachment;filename=GridViewExport.xls");
+            Response.Charset = "utf-8";
+            Response.ContentType = "application/Excel";
+            Response.AddHeader("content-disposition", "attachment;filename=GridViewExport_Ngay_"+DateTime.Now.ToString("dd-MM")+".xls");
+            //Mã hóa chữa sang UTF8
+            Response.ContentEncoding = System.Text.Encoding.UTF8;
+            Response.BinaryWrite(System.Text.Encoding.UTF8.GetPreamble());
+
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+
+            for (int i = 0; i < gv.Rows.Count; i++)
+            {
+                //Apply text style to each Row
+                gv.Rows[i].Attributes.Add("class", "textmode");
+            }
+            //Add màu nền cho header của file excel
+            gv.HeaderRow.BackColor = System.Drawing.Color.DarkOrange;
+            //Màu chữ cho header của file excel
+            gv.HeaderStyle.ForeColor = System.Drawing.Color.White;
+
+            gv.HeaderRow.Cells[0].Text = "Tên cảm biến";
+            gv.HeaderRow.Cells[1].Text = "Giá trị";
+            gv.HeaderRow.Cells[2].Text = "Tên cảm biến";
+            gv.HeaderRow.Cells[3].Text = "Giá trị";
+            gv.HeaderRow.Cells[4].Text = "Ngày cập nhật";
+            gv.RenderControl(hw);
+
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
+            return RedirectToAction("XuongTachVo");
         }
 
 
